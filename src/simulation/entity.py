81a61a246e6e036d5ef34ef6c87dc410cdbb81e5
uollouu <131377153+uollouu@users.noreply.github.com>
2025-01-18 10:54:33 +0300

    refactored classes

--- src/simulation/entity.py
+++ src/simulation/entity.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 from copy import copy
-from random import randint
 
 from colorama import Back
 
@@ -19,6 +18,7 @@ TREE_SPRITE      = Back.LIGHTGREEN_EX + " T " + Back.RESET
 
 class Entity(ABC):
 
+    @abstractmethod
     def __init__(self, map_=None, position=None):
         self.map = map_
         self.position = copy(position)
@@ -38,6 +38,9 @@ class Entity(ABC):
         else:
             self.position.set(position)
 
+    def get_neighbors(self):
+        return self.map.get_neighbors(self.position)
+
     def kill(self):
         self.map.remove(self.position)
 
@@ -49,14 +52,6 @@ class Tree(Entity):
         super().__init__(map_, position)
 
 
-class Grass(Entity):
-    sprite = GRASS_SPRITE
-
-    def __init__(self, nutrients, map_=None, position=None):
-        super().__init__(map_, position)
-        self.nutrients = nutrients
-
-
 class Rock(Entity):
     sprite = ROCK_SPRITE
 
@@ -64,28 +59,74 @@ class Rock(Entity):
         super().__init__(map_, position)
 
 
-class Creature(Entity, ABC):
-
-    def __init__(self, health, speed, scope=0, map_=None, position=None):
-        super().__init__(map_, position)
-        self.max_health = self.health = health
-        self.speed = speed
-        self.scope = scope
-        if scope == 0:
-            self.scope = self.speed * SCOPE_TO_SPEED_RATIO
-
-        self._is_alive = True
+class Food(ABC):
 
     @abstractmethod
-    def make_move(self):
+    def __init__(self, nutrients : int):
+        self.nutrients = nutrients
+
+
+class Grass(Entity, Food):
+    sprite = GRASS_SPRITE
+
+    def __init__(self, nutrients, map_=None, position=None):
+        Entity.__init__(self, map_, position)
+        Food.__init__(self, nutrients)
+
+
+class Creature(Entity, Food, ABC):
+
+    @abstractmethod
+    def __init__(self, health, speed, scope=0, map_=None, position=None):
+        Entity.__init__(self, map_, position)
+        Food.__init__(self, health)
+        self.max_health = self.health = health
+        self.speed = speed
+        self._is_alive = True
+        self.scope = scope
+
+        if self.scope == 0:
+            self.scope = self.speed * SCOPE_TO_SPEED_RATIO
+
+    @classmethod
+    @property
+    @abstractmethod
+    def target_type(cls):
+        raise NotImplementedError
+
+    @abstractmethod
+    def interact_with_target(self, target):
         pass
+
+    def make_move(self):
+        self.roam()
+
+        target = self.get_target_neighbor()
+
+        if target is not None:
+            self.interact_with_target(target)
+
+    def get_target_neighbor(self):
+        neighbors = self.get_neighbors()
+        for entity in neighbors:
+            if type(entity) == self.target_type:
+                return entity
+        return None
+
+    def roam(self):
+        path = self.map.build_path(self.position, self.scope, self.target_type)
+
+        if path:
+            self.follow_path(path)
+        else:
+            self.move_randomly()
 
     def move_to(self, new_position):
         self.map.move(self.position, new_position)
 
     def move_randomly(self):
         random_path = self.map.build_path(self.position, self.scope)
-        if random_path is not None:
+        if random_path:
             self.follow_path(random_path)
 
     def follow_path(self, path):
@@ -94,61 +135,46 @@ class Creature(Entity, ABC):
     def reduce_health(self, points):
         self.health = max(0, self.health - points)
         if self.health == 0:
-            self._is_alive = False
             self.kill()
 
     def heal(self, points):
         if not self.is_dead():
             self.health = min(self.max_health, self.health + points)
 
+    def consume(self, target):
+        self.heal(target.nutrients)
+        target.kill()
+
+    def kill(self):
+        self.health = 0
+        self._is_alive = False
+        super(Creature, self).kill()
+
     def is_dead(self):
         return not self._is_alive
 
 
+class Herbivore(Creature):
+    sprite = HERBIVORE_SPRITE
+    target_type = Grass
+
+    def __init__(self, health, speed, scope=0, map_=None, position=None):
+        super().__init__(health, speed, scope, map_, position)
+
+    def interact_with_target(self, target):
+        self.consume(target)
+
+
 class Predator(Creature):
     sprite = PREDATOR_SPRITE
+    target_type = Herbivore
 
     def __init__(self, health, speed, attack_power, scope=0, map_=None, position=None):
         super().__init__(health, speed, scope, map_, position)
         self.attack_power = attack_power
 
-    def attack(self, target):
+    def interact_with_target(self, target):
         if target.health <= self.attack_power:
-            self.heal(target.max_health)
-        target.reduce_health(self.attack_power)
-
-    def make_move(self):
-        path = self.map.build_path(self.position, self.scope, Herbivore)
-
-        if path is None:
-            self.move_randomly()
-            return
-
-        self.follow_path(path)
-
-        neighbors = self.map.get_neighbors(self.position)
-        for i in neighbors:
-            if type(i) == Herbivore:
-                self.attack(i)
-
-class Herbivore(Creature):
-    sprite = HERBIVORE_SPRITE
-
-    def __init__(self, health, speed, scope=0, map_=None, position=None):
-        super().__init__(health, speed, scope, map_, position)
-
-    def make_move(self):
-        path = self.map.build_path(self.position, self.scope, Grass)
-
-        if path is None:
-            self.move_randomly()
-            return
-
-        self.follow_path(path)
-
-        neighbors = self.map.get_neighbors(self.position)
-        for i in neighbors:
-            if type(i) == Grass:
-                target = i
-                self.heal(target.nutrients)
-                target.kill()
+            self.consume(target)
+        else:
+            target.reduce_health(self.attack_power)
