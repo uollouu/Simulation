0505e9be18b9fbc7d24b9ca60f569ea70e5bbce4
uollouu <131377153+uollouu@users.noreply.github.com>
2025-01-21 15:18:24 +0300

    add sprites.py

---
+++ src/simulation/sprites.py
@@ -0,0 +1,15 @@
+from colorama import Back, Fore
+
+from .entity import *
+
+sprites_table = {
+    type(None): Back.BLACK + Fore.BLACK + " E " + Fore.RESET + Back.RESET,
+    Predator  : Back.BLUE + " P " + Back.RESET,
+    Herbivore : Back.RED + " H " + Back.RESET,
+    Grass     : Back.GREEN + " G " + Back.RESET,
+    Rock      : Back.WHITE + Fore.WHITE + " R " + Fore.RESET + Back.RESET,
+    Tree      : Back.LIGHTGREEN_EX + Fore.LIGHTGREEN_EX + " T " + Fore.RESET+ Back.RESET,
+}
+
+def get_sprite(entity) -> str:
+    return sprites_table[type(entity)]
